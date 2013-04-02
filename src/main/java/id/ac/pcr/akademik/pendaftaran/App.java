6e4787d1f1eebeba6764e5212c7fdf9590633e12
Rika Perdana Sari <rika.perdana.s@gmail.com>
2013-04-02 09:22:48 -0700

    commit pertama

---
+++ src/main/java/id/ac/pcr/akademik/pendaftaran/App.java
@@ -0,0 +1,13 @@
+package id.ac.pcr.akademik.pendaftaran;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
