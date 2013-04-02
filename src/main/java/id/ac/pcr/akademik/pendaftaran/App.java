a2f869f5c76f85b6cbd2463eda0850c973d09534
Rika Perdana Sari <rika.perdana.s@gmail.com>
2013-04-02 09:54:51 -0700

    commit kedua

--- src/main/java/id/ac/pcr/akademik/pendaftaran/App.java
+++ src/main/java/id/ac/pcr/akademik/pendaftaran/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Rika Perdana Sari" );
+        System.out.println("Lagi Training nih... :) ");
     }
 }
